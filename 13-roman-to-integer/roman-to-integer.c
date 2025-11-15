ef94abe5675b5dc433334c4abb26c187d97516c4
rubaishaarif <rubaishaarif41@gmail.com>
2025-11-15 18:13:36 +0500

    Time: 0 ms (100.00%) | Memory: 10.4 MB (78.06%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.c
@@ -0,0 +1,32 @@
+int value(char c) {
+    switch(c) {
+        case 'I': return 1;
+        case 'V': return 5;
+        case 'X': return 10;
+        case 'L': return 50;
+        case 'C': return 100;
+        case 'D': return 500;
+        case 'M': return 1000;
+        default: return 0;
+    }
+}
+
+int romanToInt(char* s) {
+    int total = 0;
+    int i = 0;
+    
+    while (s[i] != '\0') {
+        int curr = value(s[i]);
+        int next = value(s[i+1]);
+        
+        if (next > curr) {
+            total += next - curr;
+            i += 2;
+        } else {
+            total += curr;
+            i++;
+        }
+    }
+    
+    return total;
+}

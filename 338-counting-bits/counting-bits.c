c6a7e851245c2de3135a4f14c07005b87c863bc8
rubaishaarif <rubaishaarif41@gmail.com>
2025-11-19 07:08:52 +0500

    Time: 0 ms (100.00%) | Memory: 15 MB (67.45%) - LeetSync

---
+++ 338-counting-bits/counting-bits.c
@@ -0,0 +1,12 @@
+
+int* countBits(int n, int* returnSize){
+    int *ans = (int *)malloc((n + 1) * sizeof(int));
+    ans[0] = 0;
+
+    for (int i = 1; i <= n; i++) {
+        ans[i] = ans[i >> 1] + (i & 1);
+    }
+
+    *returnSize = n + 1;
+    return ans;
+}

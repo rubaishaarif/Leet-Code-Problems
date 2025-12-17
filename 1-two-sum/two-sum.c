81f53483502f226ebce34833507ecafd9234b68a
rubaishaarif <rubaishaarif41@gmail.com>
2025-12-17 12:37:54 +0500

    Time: 101 ms (45.39%) | Memory: 8.6 MB (93.11%) - LeetSync

--- 1-two-sum/two-sum.c
+++ 1-two-sum/two-sum.c
@@ -1,24 +1,20 @@
-
-
-int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
- 
-    int* result = (int*)malloc(2 * sizeof(int));
-
-    *returnSize = 2;
-
-    for (int i = 0; i < numsSize - 1; i++) {
-
-        for (int j = i + 1; j < numsSize; j++) {
- 
-            if (nums[i] + nums[j] == target) {
-   
-                result[0] = i;
-                result[1] = j;
-          
-                return result;
-            }
-        }
+//2 sum using pointers
+int *twoSum(int *nums, int numsize, int target , int *returnsize)
+{
+*returnsize=2;
+int *result = (int*) malloc(2 * sizeof(int));
+int i,j;
+for(i=0;i<numsize;i++)
+{
+    for(j=i+1;j<numsize;j++)
+    {
+        if(nums[i]+nums[j]== target) {
+        result[0] = i;
+        result[1] = j;
+        return result;    }
     }
 
-    return result; 
+}
+return result;
+
 }
